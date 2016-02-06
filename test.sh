e0f003d189c92d0770b3ff48554048d08b78c0cc
Markus Tacker <m@cto.hiv>
2016-02-06 23:34:27 +0100

    Fix tests with HTML proof.

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+function killJekyll {
+    SERVER_PID=`lsof -i -n -P | grep jekyll | grep 127.0.0.1:4000 | awk '{ print $2; }'`
+    if [ $SERVER_PID ];
+    then
+        kill $SERVER_PID
+    fi
+}
+
+function finish {
+    STATUS=$?
+    killJekyll
+    exit $STATUS
+}
+trap finish EXIT
+
+jekyll build
+jekyll serve -B -P 4000 -H 127.0.0.1 -c _config.yml,_config.travis.yml
+
+htmlproof ./_site
+
+killJekyll
